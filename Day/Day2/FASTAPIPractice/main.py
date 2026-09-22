0ad6bd23ab5155f3cd8975d031ebbc08d378aae4
vishal <2025cs_baddikrishnavishal_b@nie.ac.in>
2026-09-22 12:55:18 +0530

    create multiple routes

--- Day/Day2/FASTAPIPractice/main.py
+++ Day/Day2/FASTAPIPractice/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 app=FastAPI()
 @app.get("/")
-def read_root():
-    return {"message":"Hello World","number":44,"is_fun":True}
+def home():
+    return {"page":"Home"}
+@app.get("/about")
+def about():
+    return {"page":"About","author":"Rakesh"}
+@app.get("/health")
+def health():
+    return{"status":"ok"}
